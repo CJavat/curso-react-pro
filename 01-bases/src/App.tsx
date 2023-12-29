9cf5434129d2e4d1d8a2711a1ad59a21e773fd2e
CJavat <cdpm98@hotmail.com>
2023-12-29 16:10:11 -0600

    SECCIÓN 2 / CAP 17 - EN CURSO

---
+++ 01-bases/src/App.tsx
@@ -0,0 +1,16 @@
+import { Counter } from "./bases/Counter";
+import { CounterBy } from "./bases/CounterBy";
+import { CounterEffect } from "./bases/CounterEffect";
+
+function App() {
+  
+  return (
+    <>
+      <Counter />
+      <CounterBy />
+      <CounterEffect />
+    </>
+  );
+}
+
+export default App;
